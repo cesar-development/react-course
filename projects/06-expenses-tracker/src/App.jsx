3098d21a88a642f2cca12095e5fea03119541d5d
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-08-14 13:42:23 -0600

    feat(projects/06-expenses-tracker): Add static icons and style files

--- projects/06-expenses-tracker/src/App.jsx
+++ projects/06-expenses-tracker/src/App.jsx
@@ -1,5 +1,10 @@
+import icon from './assets/icons/delete.svg'
+
 export default function App() {
   return (
-    <h1>Hola Mundo!!!</h1>
+    <>
+      <h1>Hola Mundo!!!</h1>
+      <img src={icon} alt="icono" />
+    </>
   )
 }
