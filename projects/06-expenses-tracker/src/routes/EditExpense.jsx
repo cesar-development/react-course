6d9140faaebc7a87a702a473f8f7638d905e9710
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-08-16 14:33:15 -0600

    feat(projects/06-expenses-tracker): Add Edit Expense Route

--- projects/06-expenses-tracker/src/routes/EditExpense.jsx
+++ projects/06-expenses-tracker/src/routes/EditExpense.jsx
@@ -1,5 +1,30 @@
+import { Helmet } from 'react-helmet'
+import { useParams } from 'react-router-dom'
+
+import ExpenseForm from '../components/ExpenseForm'
+import ReturnButton from '../components/ReturnButton'
+import TotalExpenseBar from '../components/TotalExpenseBar'
+
+import useExpenseById from '../hooks/useExpenseById.js'
+import { Header, Title } from '../styled-elements/Header'
+
 export default function EditExpense() {
+  const { id } = useParams()
+  const { expense } = useExpenseById({ id })
+
   return (
-    <div>EditExpense</div>
+    <>
+      <Helmet>
+        <title>Editar Gasto</title>
+      </Helmet>
+
+      <Header>
+        <ReturnButton route='/list' />
+        <Title>Editar Gasto</Title>
+      </Header>
+
+      <ExpenseForm expense={expense} />
+      <TotalExpenseBar />
+    </>
   )
 }
