4b7e252cf948ffb31a82b53afdd25996b280af05
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-08-16 14:32:58 -0600

    feat(projects/06-expenses-tracker): Add Expenses by Category Route

--- projects/06-expenses-tracker/src/routes/ExpenseByCategory.jsx
+++ projects/06-expenses-tracker/src/routes/ExpenseByCategory.jsx
@@ -1,5 +1,43 @@
+import { Helmet } from 'react-helmet'
+
+import CategoryIcons from '../components/CategoryIcons'
+import ReturnButton from '../components/ReturnButton'
+import TotalExpenseBar from '../components/TotalExpenseBar'
+
+import { Header, Title } from '../styled-elements/Header'
+import { CategoryList, CategoryListElement, Category, Value } from '../styled-elements/ListElements'
+
+import useExpensesByCategory from '../hooks/useExpensesByCategory.js'
+import { currencyFormatter } from '../helpers/currency-formatter.js'
+
 export default function ExpenseByCategory() {
+  const { expensesByCategory } = useExpensesByCategory()
+
   return (
-    <div>ExpenseByCategory</div>
+    <>
+      <Helmet>
+        <title>Gastos por Categoría</title>
+      </Helmet>
+
+      <Header>
+        <ReturnButton />
+        <Title>Gastos por Categoría</Title>
+      </Header>
+
+      <CategoryList>
+        {expensesByCategory.map((category) => (
+          <CategoryListElement key={category.category}>
+            <Category>
+              <CategoryIcons id={category.category} />
+              {category.category}
+            </Category>
+
+            <Value>{currencyFormatter(category.quantity)}</Value>
+          </CategoryListElement>
+        ))}
+      </CategoryList>
+
+      <TotalExpenseBar />
+    </>
   )
 }
